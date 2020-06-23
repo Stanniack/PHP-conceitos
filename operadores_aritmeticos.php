24c8bb6eb2ee0f50466b2c154b36778df1f24998
Mateus Vitor <stanniack@gmail.com>
2020-06-23 02:35:51 -0300

    Create operadores_aritmeticos.php
    Ponto (.) equivale a mesma coisa que "+" nos SOUTs.

---
+++ operadores_aritmeticos.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Olá mundo</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+
+<body>
+<div>
+    <?php
+        $n1 = 11;
+        $n2 = 10;
+        $r1 = $n1 + $n2;
+        $r2 = $n1 - $n2;
+        $r3 = $n1 * $n2;
+        $r4 = $n1 / $n2;
+        $r5 = $n1 % $n2;
+
+        echo "A soma é: ".($n1 + $n2) . "<br>";
+        echo "A soma é: $r1";
+
+    ?>
+</div>
+</body>
+
+</html>
